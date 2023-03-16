15a3263fad5dbc19437f8ffe891649847c7524a6
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-16 01:11:45 -0700

    5 hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(){
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to the standard output
+ * using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
