a4338d2e6410052ac8c91033c61a27552c5e68cd
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-16 01:31:32 -0700

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+* main - Entry point of the program
+*
+* Description: Prints a message to the standard error.
+* Return: 1
+*/
+int main(void)
+{
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = 0;
+while (msg[len])
+  {
+    len++;
+  }
+
+write(2, msg, len);
+return (1);
+}
