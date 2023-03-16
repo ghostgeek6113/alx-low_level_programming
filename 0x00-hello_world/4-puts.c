4a0c098fc3c9c609d79846b76d68be88edf067c3
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-16 01:04:13 -0700

    4 hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main(void) 
+int main(void)
 {
 /*
-   This program prints a message to the screen using the puts function.
-   It takes no input parameters and returns 0 when it is finished.
+*This program prints a message to the screen using the puts function.
+*It takes no input parameters and returns 0 when it is finished.
 */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
