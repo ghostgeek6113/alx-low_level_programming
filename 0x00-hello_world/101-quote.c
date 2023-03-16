27386888dff80777f04de261a3ce3ddd501f2bf1
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-16 01:33:39 -0700

    8 hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,9 @@ int main(void)
 char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 int len = 0;
 while (msg[len])
-  {
-    len++;
-  }
+{
+len++;
+}
 
 write(2, msg, len);
 return (1);
