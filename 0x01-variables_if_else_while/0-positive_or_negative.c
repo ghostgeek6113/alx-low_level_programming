f5418998690a8c40ccf2c075331525e655821b5f
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-17 13:49:41 -0700

    0 hot fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,14 +16,17 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
+printf("\n");
 }
 else if (n == 0)
 {
 printf("%d is zero\n", n);
+printf("\n");
 }
 else if (n < 0)
 {
 printf("%d is negative\n", n);
+printf("\n");
 }
 return (0);
 }
