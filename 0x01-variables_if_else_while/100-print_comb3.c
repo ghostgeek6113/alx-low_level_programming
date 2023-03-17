a57b01d240d9133feb1460d503cc680acf3a2b9a
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-17 15:16:56 -0700

    10 hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,31 +8,21 @@
 
 int main(void)
 {
-    int i, j;
-
-    for (i = 0; i < 99; i++)
-    {
-        for (j = i + 1; j < 100; j++)
-        {
-            if (i != j)
-            {
-                putchar((i / 10) + '0');
-                putchar((i % 10) + '0');
-                putchar(',');
-                putchar(' ');
-                putchar((j / 10) + '0');
-                putchar((j % 10) + '0');
-
-                if (i < 98 || j < 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-
-putchar('\n');
-
-return (0);
+int i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
 }
+}
+}
+putchar('\n');
+return 0;
+}
+
