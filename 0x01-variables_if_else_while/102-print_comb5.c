f6074b9f18ed447a3375abec8123a2e117c99de4
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-17 15:28:50 -0700

    12 hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,26 +14,25 @@
 
 int main(void)
 {
-    int num1, num2;
+int num1, num2;
 
-    for (num1 = 0; num1 < 100; num1++)
-    {
-        for (num2 = num1 + 1; num2 < 100; num2++)
-        {
-            putchar((num1 / 10) + '0');
-            putchar((num1 % 10) + '0');
-            putchar(' ');
-            putchar((num2 / 10) + '0');
-            putchar((num2 % 10) + '0');
+for (num1 = 0; num1 < 100; num1++)
+{
+for (num2 = num1 + 1; num2 < 100; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
 
-            if (num1 != 98 || num2 != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-    return (0);
+if (num1 != 98 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
