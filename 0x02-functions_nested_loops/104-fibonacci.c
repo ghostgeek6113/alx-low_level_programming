b603cd1a6c529470b3dd6ddb145aeab18c6370db
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-21 13:52:43 -0700

    16 hot fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,34 @@
 */
 int main(void)
 {
-unsigned long a = 1, b = 2, c;
-int i;
-printf("%lu, %lu, ", a, b);
-for (i = 0; i < 96; i++)
+unsigned long int i, j, k, j1, j2, k1, k2;
+
+j = 1;
+k = 2;
+
+printf("%lu", j);
+
+for (i = 1; i < 91; i++)
 {
-c = a + b;
-printf("%lu", c);
-if (i != 96)
+printf(", %lu", k);
+k = k + j;
+j = k - j;
+}
+
+j1 = j / 1000000000;
+j2 = j % 1000000000;
+k1 = k / 1000000000;
+k2 = k % 1000000000;
+for (i = 92; i < 99; ++i)
 {
-printf(", ");
+printf(", %lu", k1 + (k2 / 1000000000));
+printf("%lu", k2 % 1000000000);
+k1 = k1 + j1;
+j1 = k1 - j1;
+k2 = k2 + j2;
+j2 = k2 - j2;
 }
-a = b;
-b = c;
-}
+
 printf("\n");
 return (0);
 }
