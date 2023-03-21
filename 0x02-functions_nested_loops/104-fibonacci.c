c7c137945ce1b64778ee3fa08c003a7f6cb11358
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-21 12:58:31 -0700

    16 hot fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,11 +11,11 @@ int main(void)
 unsigned long a = 1, b = 2, c;
 int i;
 printf("%lu, %lu, ", a, b);
-for (i = 0; i < 48; i++)
+for (i = 0; i < 98; i++)
 {
 c = a + b;
 printf("%lu", c);
-if (i != 47)
+if (i != 98)
 {
 printf(", ");
 }
