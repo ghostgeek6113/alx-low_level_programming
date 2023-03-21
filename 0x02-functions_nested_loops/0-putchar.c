4cb0e44e12638c16945cf3e5451b9900b8333c7a
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-21 09:39:38 -0700

    main.h added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include "main.h"
 /*
 * main: entry point
 *
@@ -7,6 +7,15 @@
 */
 int main(void)
 {
-printf("_putchar");
-return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+def  HOLBERTON_H_INCLUDED
+line HOLBERTON_H_INCLUDED
+
+_putchar(char)
+ print_alphabet_x10(void);
+ print_alphabet_x10(void);
+_islower(int c);
+_isalpha(int c);
+print_sign(int n);
+_abs(int);
+print_last_digit(int);
+ jack_bauer(void);
+ times_table(void);
+add(int, int);
+ print_to_98(int n);
+ print_times_table(int n);
+ num(int r, int n);
