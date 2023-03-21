6f9993b3a82e20092c035ab4e31d0335b3d51c74
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-21 11:52:08 -0700

    12 hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,8 +12,6 @@ if (n < 0 || n > 15)
 {
 return;
 }
-
-
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -36,7 +34,6 @@ if (result < 10)
 {
 _putchar(' ');
 }
-
 if (result >= 100)
 {
 _putchar(result / 100 + '0');
