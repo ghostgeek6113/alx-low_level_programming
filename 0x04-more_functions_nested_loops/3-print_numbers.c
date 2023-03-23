3f3024c1b892300081f3b5c0ef6584bb3cc5aede
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-23 15:26:46 -0700

    3 hot fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
+
 /**
-* print_numbers - print 0-9
-*/
+ * print_numbers - print numbers 0 to 9
+ */
 void print_numbers(void)
 {
-int c;
-for (c = '0'; c <= '9';c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+		_putchar(n);
+	_putchar('\n');
 }
