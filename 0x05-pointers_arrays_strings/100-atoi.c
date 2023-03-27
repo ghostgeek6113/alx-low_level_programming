c2c0e0f647b59164cb9e66483bebbc24f1c43710
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-03-27 14:47:23 -0700

    10 hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,51 +1,24 @@
 #include "main.h"
-#include <limits.h>
-
 /**
- * _atoi - convert a string to an integer
- * @s: the string to convert
- *
- * Return: result of conversion
- */
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
 int _atoi(char *s)
 {
-	unsigned int n = 0;
-	int sign = 1;
-	int flag = 0;
+	int i = 1;
 
-	while (*s)
-	{
-		if (*s >= '0' && *s <= '9')
-		{
-			flag = 1;
-			if (sign > 0)
-			{
-				if (INT_MAX / 10 < (int) n)
-					return (INT_MAX);
-				n *= 10;
-				if (INT_MAX - (*s - '0') < (int) n)
-					return (INT_MAX);
-			}
-			else
-			{
-				if (INT_MIN / 10 < (int) -n)
-					return (INT_MIN);
-				n *= 10;
-				if (INT_MIN + (*s - '0') > (int) -n)
-					return (INT_MIN);
-			}
-			n += (*s - '0');
-		}
-		else if (flag)
-		{
-			break;
-		}
-		else if (*s == '-')
-		{
-			sign *= -1;
-		}
-		++s;
-	}
+	unsigned int num = 0;
 
-	return (n * sign);
+	do {
+	if (*s == '-')
+	i *= -1;
+	else if (*s >= '0' && *s <= '9')
+	num = num * 10 + (*s - '0');
+	else if (num > 0)
+	break;
+	} while (*s++);
+
+	return (num * i);
 }
