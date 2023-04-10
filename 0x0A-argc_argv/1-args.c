8e5066f20fd3292fc756d36b3419d27a6b748ad4
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-10 11:39:20 -0700

    Week 5 C Argv and Argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argv: The character to print
+ * @argc: The character to print
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Week 5 C Argv and Argc
