d7d7102fa7f4b4a77d4c8362e820b699a0e370c9
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-11 10:54:44 -0700

    3 Hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,8 +30,8 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++
-				grid[i][j] = 0;
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 
 	return (grid);
