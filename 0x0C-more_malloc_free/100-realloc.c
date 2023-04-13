f4a6560753f76647dc63fcd4e1a71b7c6e31d027
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-13 13:22:20 -0700

    4 Hot fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,12 +17,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	
 	if (new_size == old_size)
 		return (ptr);
-	
+
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
-		
 	}
 
 	if (!ptr)
@@ -38,14 +37,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		for (i = 0; i < new_size; i++)
 			ptr1[i] = old_ptr[i];
-		
 	}
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-		
 			ptr1[i] = old_ptr[i];
-	}		
-	free(ptr);	
-	return (ptr1);		
+	}
+	free(ptr);
+	return (ptr1);
 }
