8b5a5c5565dc4a67e186e632cbbef11c6729ea5e
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-17 09:49:45 -0700

    Week 7 C - Structures, typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Entry point
+ * @d: dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
