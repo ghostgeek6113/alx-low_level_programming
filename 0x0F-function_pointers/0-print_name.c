6180d95ffbeae0d264da75746eb2c7aa05cdf0be
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-19 12:34:15 -0700

    0 Hot fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - print name using pointer to function
@@ -7,9 +7,11 @@
  * @f: pointer to function
  * Return: 
  **/
+
 void print_name(char *name, void (*f)(char *))
 {
-	if(name == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
+	
 	f(name);
 }
