3ac33ac73e9eccc3f342dfa7e5674edb917b07c0
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-19 12:49:19 -0700

    2 Hot fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,13 +5,13 @@
  * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
- * Return: 
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	
+
 	f(name);
 }
