f22ed3e4298bba3e8747c05046de1b730832131f
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-19 12:31:04 -0700

    0 Hot fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,7 @@
  * @f: pointer to function
  * Return: 
  **/
-void print_name(char *name, void (*f)()char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if(name == NULL || f == NULL)
 		return;
