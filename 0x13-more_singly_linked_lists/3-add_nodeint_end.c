d300a39e2bf34434d144242bdf9b4559c869b48b
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-05-02 10:49:44 -0700

    3 hot fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
-	listint_t *tmp_node;
+	listint_t *tmp_node = *head;
 
 	new_node = malloc(sizeof(listint_t));
 
@@ -32,9 +32,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (new_node);
 	}
 
-	while (*head-> next)
-		*head = *head->next;
+	while (tmp_node->next)
+		tmp_node =tmp_node->next;
 
-	*head->next = new_node;
+	tmp_node->next = new_node;
 	return (new_node);
 }
